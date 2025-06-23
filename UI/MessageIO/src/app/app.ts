67b61ffb7fbd7bd0c4881b606e103b6456dbe67b
SirJohnGabriel <j.g.pagtalunan14@gmail.com>
2025-06-23 14:45:29 +0800

    frontend login, dashboard, logout, session/token management, registration

--- UI/MessageIO/src/app/app.ts
+++ UI/MessageIO/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.models';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule ],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- UI/MessageIO/src/app/components/login/login.ts
+++ UI/MessageIO/src/app/components/login/login.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule, RouterLink ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-  mapConnect = '/connect_map2.png';
-  mapAlt = 'Map Connect';
+  http = inject(HttpClient);
+  router = inject(Router);
+  
+  loginForm = new FormGroup({
+    identifier: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onLoginSubmit() {
+    const payload = this.loginForm.value;
+
+    this.http.post<any>('https://localhost:7218/api/user/login', payload).subscribe({
+      next: (response) => {
+        console.log("Login Success", response);
+
+        localStorage.setItem('token', response.token);
+
+        this.router.navigate(['/dashboard'])
+      },
+      error: (err) => {
+        console.log("Login failed", err);
+        alert("Invalid Credentials");
+      }
+    });
+  }
 }
